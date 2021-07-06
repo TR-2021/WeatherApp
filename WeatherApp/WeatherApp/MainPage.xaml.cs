c2da6bb70967019ddbb21919e4e6214bb9bc7740
Stas Bes <forgame98@yandex.ru>
2021-07-06 12:37:01 +0300

    1st stable versio

--- WeatherApp/WeatherApp/MainPage.xaml.cs
+++ WeatherApp/WeatherApp/MainPage.xaml.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using WeatherApp.Converter;
+﻿using System.Collections.ObjectModel;
 using WeatherApp.Views;
 using Xamarin.Forms;
 using static WeatherApp.Converter.ForecastEnumToImageConverter;
@@ -13,16 +7,66 @@ namespace WeatherApp
 {
     public partial class MainPage : ContentPage
     {
-        public static readonly BindableProperty CurrentWeatherProperty = BindableProperty.Create("CurrentWeather", typeof(ForecastEnum), typeof(MainPage), ForecastEnum.Thunder);
-        public ForecastEnum CurrentWeather
-        {
-            get { return (ForecastEnum)GetValue(CurrentWeatherProperty); }
-            set { SetValue(CurrentWeatherProperty, value); }
-        }
+        //public static readonly BindableProperty CurrentWeatherProperty = BindableProperty.Create("CurrentWeather", typeof(ForecastEnum), typeof(MainPage), ForecastEnum.Thunder);
+        public ObservableCollection<Weather> WeatherStateList { get; set; } = new ObservableCollection<Weather>();
+        public Weather CurrentWeather { get; set; } = new Weather();
         public MainPage()
         {
+            WeatherStateList.Add(new Weather() { Temperature = "52", Time = "Now", Humidity = "10", Day = "Monday", WeatherType = ForecastEnum.Cloudy });
+            WeatherStateList.Add(new Weather() { Temperature = "12", Time = "1AM", Humidity = "20", Day = "Tuesday", WeatherType = ForecastEnum.Sunny });
+            WeatherStateList.Add(new Weather() { Temperature = "22", Time = "2AM", Humidity = "30", Day = "Wednesday", WeatherType = ForecastEnum.Thunder });
+            WeatherStateList.Add(new Weather() { Temperature = "42", Time = "3AM", Humidity = "30", Day = "Thursday", WeatherType = ForecastEnum.Rain });
+            WeatherStateList.Add(new Weather() { Temperature = "52", Time = "4AM", Humidity = "40", Day = "Friday", WeatherType = ForecastEnum.Sunny });
+            WeatherStateList.Add(new Weather() { Temperature = "62", Time = "5AM", Humidity = "50", Day = "Saturday", WeatherType = ForecastEnum.Sunny });
+            WeatherStateList.Add(new Weather() { Temperature = "72", Time = "6AM", Humidity = "50", Day = "Sunday", WeatherType = ForecastEnum.Sunny });
+            WeatherStateList.Add(new Weather() { Temperature = "32", Time = "7AM", Humidity = "50", Day = "Monday", WeatherType = ForecastEnum.Sunny });
+            WeatherStateList.Add(new Weather() { Temperature = "52", Time = "8AM", Humidity = "50", Day = "Tuesday", WeatherType = ForecastEnum.Sunny });
+            WeatherStateList.Add(new Weather() { Temperature = "72", Time = "9AM", Humidity = "50", Day = "Wednesday", WeatherType = ForecastEnum.Sunny });
+            WeatherStateList.Add(new Weather() { Temperature = "32", Time = "10AM", Humidity = "50", Day = "Thursday", WeatherType = ForecastEnum.Sunny });
+            CurrentWeather = WeatherStateList[0];
             InitializeComponent();
             BindingContext = this;
+            weatherCollectionView.BindingContext = this;
+            weatherCollectionView.SetBinding(ItemsView.ItemsSourceProperty, "WeatherStateList");
+            weatherCollectionView.ItemTemplate = new DataTemplate(() =>
+            {
+                WeatherItem item = new WeatherItem();
+                item.ItemDirection = FlexDirection.Column;
+                item.HumidityVisible = false;
+                item.TextColor = Color.White;
+                item.SetBinding(WeatherItem.TemperatureProperty, "Temperature");
+                item.SetBinding(WeatherItem.WeatherTypeProperty, "WeatherType");
+                item.SetBinding(WeatherItem.WeatherTimeProperty, "Time");
+                return item;
+            });
+
+            weatherListView.BindingContext = this;
+            weatherListView.SetBinding(ListView.ItemsSourceProperty, "WeatherStateList");
+            weatherListView.ItemTemplate = new DataTemplate(() =>
+            {
+                WeatherItem item = new WeatherItem();
+                item.ItemDirection = FlexDirection.Row;
+                item.TextColor = Color.White;
+                item.SetBinding(WeatherItem.TemperatureProperty, "Temperature");
+                item.SetBinding(WeatherItem.WeatherTypeProperty, "WeatherType");
+                item.SetBinding(WeatherItem.WeatherTimeProperty, "Day");
+                return new ViewCell
+                {
+                    View = item
+                };
+            });
+        }
+
+
+        public class Weather
+        {
+            public string Time { get; set; } = "NOW";
+            public string Location { get; set; } = "Cupertino";
+            public ForecastEnum WeatherType { get; set; } = ForecastEnum.Sunny;
+            public string Temperature { get; set; } = "123";
+            public string Day { get; set; } = "Monday";
+            public string Humidity { get; set; } = "55";
+
         }
     }
 }
--- WeatherApp/WeatherApp/Views/WeatherItem.xaml.cs
+++ WeatherApp/WeatherApp/Views/WeatherItem.xaml.cs
@@ -16,18 +16,51 @@ namespace WeatherApp.Views
         public static readonly BindableProperty WeatherTimeProperty = BindableProperty.Create("WeatherTime", typeof(string), typeof(WeatherItem), "", propertyChanged: WeatherTimerPropertyChanged);
         public static readonly BindableProperty WeatherTypeProperty = BindableProperty.Create("WeatherType", typeof(ForecastEnum), typeof(WeatherItem), ForecastEnum.Thunder, propertyChanged: WeatherTypePropertyChanged);
         public static readonly BindableProperty TemperatureProperty = BindableProperty.Create("Temperature", typeof(string), typeof(WeatherItem), "", propertyChanged: TemperaturePropertyChanged);
-        public static readonly BindableProperty ItemDirectionProperty = BindableProperty.Create("ItemDirection", typeof(FlexDirection), typeof(WeatherItem), FlexDirection.Column, propertyChanged: ItemDirectionPropertyChanged);
+        public static readonly BindableProperty HumidityProperty = BindableProperty.Create("Humidity", typeof(string), typeof(WeatherItem), "", propertyChanged: HumidityPropertyChanged);
+        public static readonly BindableProperty ItemDirectionProperty = BindableProperty.Create("ItemDirection", typeof(FlexDirection), typeof(WeatherItem), FlexDirection.Row, propertyChanged: ItemDirectionPropertyChanged);
+        public static readonly BindableProperty HumidityVisibleProperty = BindableProperty.Create("HumidityVisible", typeof(bool), typeof(WeatherItem), true, propertyChanged: HumidityVisiblePropertyChanged);
+        public static readonly BindableProperty TextColorProperty = BindableProperty.Create("TextColor", typeof(Color), typeof(WeatherItem), Color.AliceBlue, propertyChanged: TextColorPropertyChanged);
 
-      
-        public string WeatherTime { get { return (string)GetValue(WeatherTimeProperty);} set { SetValue(WeatherTimeProperty, value); } }
-        public ForecastEnum WeatherType { get { return (ForecastEnum)GetValue(WeatherTypeProperty); } set { SetValue(WeatherTypeProperty, value); } }
-        public string Temperature { get { return (string)GetValue(TemperatureProperty); } set { SetValue(TemperatureProperty, value); } }
-        public FlexDirection ItemDirection { get { return (FlexDirection)GetValue(ItemDirectionProperty); } set { SetValue(ItemDirectionProperty, value); } }
-
+       
+        public string WeatherTime 
+        {
+            get { return (string)GetValue(WeatherTimeProperty); } 
+            set { SetValue(WeatherTimeProperty, value); } 
+        }
+        public ForecastEnum WeatherType 
+        { 
+            get { return (ForecastEnum)GetValue(WeatherTypeProperty); } 
+            set { SetValue(WeatherTypeProperty, value); } 
+        }
+        public string Temperature
+        {
+            get { return (string)GetValue(TemperatureProperty); }
+            set { SetValue(TemperatureProperty, value); }
+        }
+        public string Humidity
+        {
+            get { return (string)GetValue(HumidityProperty); }
+            set { SetValue(HumidityProperty, value); }
+        }
+        public bool HumidityVisible
+        {
+            get { return (bool)GetValue(HumidityVisibleProperty); }
+            set { SetValue(HumidityVisibleProperty, value); }
+        }
+        public FlexDirection ItemDirection 
+        { 
+            get { return (FlexDirection)GetValue(ItemDirectionProperty); } 
+            set { SetValue(ItemDirectionProperty, value); } 
+        }
+        public Color TextColor
+        {
+            get { return (Color)GetValue(TextColorProperty); }
+            set { SetValue(TextColorProperty, value); }
+        }
         public WeatherItem()
         {
             InitializeComponent();
-            BindingContext = this;
+            //BindingContext = this;
         }
 
         private static void WeatherTimerPropertyChanged(BindableObject bindable, object oldValue, object newValue)
@@ -45,6 +78,22 @@ namespace WeatherApp.Views
         private static void ItemDirectionPropertyChanged(BindableObject bindable, object oldValue, object newValue)
         {
             (bindable as WeatherItem).flexLayout.Direction = (FlexDirection)newValue;
+        }
+        private static void HumidityPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            (bindable as WeatherItem).humidityLabel.Text= (string) newValue;
+        }
+        private static void HumidityVisiblePropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            (bindable as WeatherItem).humidityLabel.SetValue(Label.IsVisibleProperty,newValue);
+
+        }
+
+        private static void TextColorPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            (bindable as WeatherItem).humidityLabel.SetValue(Label.TextColorProperty, newValue);
+            (bindable as WeatherItem).temperatureLabel.SetValue(Label.TextColorProperty, newValue);
+            (bindable as WeatherItem).timeLabel.SetValue(Label.TextColorProperty, newValue);
         }
 
     }
