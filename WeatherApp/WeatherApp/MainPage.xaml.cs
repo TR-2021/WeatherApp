f2fa0ee1de9a512b0e128a485302ab863dfa8e73
Stas Bes <forgame98@yandex.ru>
2021-07-06 16:46:57 +0300

    refactored

--- WeatherApp/WeatherApp/MainPage.xaml.cs
+++ WeatherApp/WeatherApp/MainPage.xaml.cs
@@ -12,6 +12,13 @@ namespace WeatherApp
         public Weather CurrentWeather { get; set; } = new Weather();
         public MainPage()
         {
+            PupulateList();
+            InitializeComponent();
+            BindingContext = this;
+        }
+
+        private void PupulateList()
+        {
             WeatherStateList.Add(new Weather() { Temperature = "52", Time = "Now", Humidity = "10", Day = "Monday", WeatherType = ForecastEnum.Cloudy });
             WeatherStateList.Add(new Weather() { Temperature = "12", Time = "1AM", Humidity = "20", Day = "Tuesday", WeatherType = ForecastEnum.Sunny });
             WeatherStateList.Add(new Weather() { Temperature = "22", Time = "2AM", Humidity = "30", Day = "Wednesday", WeatherType = ForecastEnum.Thunder });
@@ -24,39 +31,7 @@ namespace WeatherApp
             WeatherStateList.Add(new Weather() { Temperature = "72", Time = "9AM", Humidity = "50", Day = "Wednesday", WeatherType = ForecastEnum.Sunny });
             WeatherStateList.Add(new Weather() { Temperature = "32", Time = "10AM", Humidity = "50", Day = "Thursday", WeatherType = ForecastEnum.Sunny });
             CurrentWeather = WeatherStateList[0];
-            InitializeComponent();
-            BindingContext = this;
-            weatherCollectionView.BindingContext = this;
-            weatherCollectionView.SetBinding(ItemsView.ItemsSourceProperty, "WeatherStateList");
-            weatherCollectionView.ItemTemplate = new DataTemplate(() =>
-            {
-                WeatherItem item = new WeatherItem();
-                item.ItemDirection = FlexDirection.Column;
-                item.HumidityVisible = false;
-                item.TextColor = Color.White;
-                item.SetBinding(WeatherItem.TemperatureProperty, "Temperature");
-                item.SetBinding(WeatherItem.WeatherTypeProperty, "WeatherType");
-                item.SetBinding(WeatherItem.WeatherTimeProperty, "Time");
-                return item;
-            });
-
-            weatherListView.BindingContext = this;
-            weatherListView.SetBinding(ListView.ItemsSourceProperty, "WeatherStateList");
-            weatherListView.ItemTemplate = new DataTemplate(() =>
-            {
-                WeatherItem item = new WeatherItem();
-                item.ItemDirection = FlexDirection.Row;
-                item.TextColor = Color.White;
-                item.SetBinding(WeatherItem.TemperatureProperty, "Temperature");
-                item.SetBinding(WeatherItem.WeatherTypeProperty, "WeatherType");
-                item.SetBinding(WeatherItem.WeatherTimeProperty, "Day");
-                return new ViewCell
-                {
-                    View = item
-                };
-            });
         }
-
 
         public class Weather
         {
